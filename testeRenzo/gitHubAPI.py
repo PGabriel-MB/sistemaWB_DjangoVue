4b5478329df713e962f8b359435b2e8a7eb4439d
PGabriel-MB <pablogrbarbosa@gmail.com>
2019-12-14 13:27:13 -0300

    Criando e preparando ambiente com CI acampanhando curso Renzo Python Pro

---
+++ testeRenzo/gitHubAPI.py
@@ -0,0 +1,15 @@
+import requests
+
+def buscar_avatar(nome):
+    '''
+    - acompanhamneto do curso Renzo Python Pro - 
+    esta função vai er eliminada logo a frente
+
+    '''
+     
+    url = f'https://api.github.com/users/{nome}'
+    resposta = requests.get(url)
+    return resposta.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('PGabriel-MB'))
