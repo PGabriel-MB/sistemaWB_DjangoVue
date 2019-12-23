a74c1a90bed89c871358bf84770e056a8a5acad5
PGabriel-MB <pablogrbarbosa@gmail.com>
2019-12-23 20:17:00 -0300

    aula de testes do freamework

--- testeRenzo/tests/test_spam/test_enviador_de_email.py
+++ testeRenzo/tests/test_spam/test_enviador_de_email.py
@@ -1,9 +1,11 @@
 from spam.enviador_de_email import Enviador
+from spam.enviador_de_email import EmailInvalido
 import pytest
 
 def test_criar_enviador_email():
     enviador = Enviador()
     assert enviador is not None
+
 
 @pytest.mark.parametrize(
     'destinatario',
@@ -18,3 +20,18 @@ def test_remetente(destinatario):
         'Primeiro teste de remetente imaginário'
     )
     assert resultado in resultado
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['','pablo' ]
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        resultado = enviador.enviar(
+            remetente,
+            'alchepablomist@gmail.com',
+            'Teste de remetente - Python Pro',
+            'Primeiro teste de remetente imaginário'
+        )
+    #assert resultado in resultado
