6b60e187042a7a6e42eba743f861a7484d17a676
PGabriel-MB <pablogrbarbosa@gmail.com>
2020-04-23 12:04:19 -0300

    inserida configuraçao de ambiente de desenvolvimento e de produçao pelo ttravis e pelo python-decouple

--- sistemaWB/settings.py
+++ sistemaWB/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+from decouple import config
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(h57bce^%&(cy^q)pt=inl0s&v!n*iqt!#+*atx#k-at2=7m#6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config ('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
