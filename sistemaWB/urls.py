1f1d905515f4f48848deed74729217c3e812117d
PGabriel-MB <pablogrbarbosa@gmail.com>
2020-04-23 01:29:53 -0300

    Ola django

--- sistemaWB/urls.py
+++ sistemaWB/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .base.views import home
+from sistemaWB.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('',)
+    path('', home)
 ]
