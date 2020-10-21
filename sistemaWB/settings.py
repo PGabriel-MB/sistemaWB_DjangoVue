d1ee5d2b52ba51f0e30842267ca715f176b2a3d4
PGabriel-MB <pablogrbarbosa@gmail.com>
2020-10-20 23:20:44 -0300

    testanto settings para arquivos estaticos

--- sistemaWB/settings.py
+++ sistemaWB/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, STATIC_URL)
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 MEDIA_URL = '/mediafiles/'
